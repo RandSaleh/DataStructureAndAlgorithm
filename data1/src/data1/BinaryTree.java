c9f460973fcd5b46e70ba98d9b70b601f9700d47
RandSaleh <rand_nn@hotmail.com>
2019-02-17 20:03:50 +0200

    add the inorder postorder preorder

--- data1/src/data1/BinaryTree.java
+++ data1/src/data1/BinaryTree.java
@@ -18,7 +18,7 @@ public class BinaryTree {
     public void insert(int data){
     
     if (this.data < data){
-    if (node.LeftNode == null){
+    if (LeftNode == null){
     LeftNode = new Node(data);
     }
     else 
@@ -28,11 +28,89 @@ public class BinaryTree {
     
     
     if (this.data>data){
-    
+    if (RightNode == null ){
+    RightNode = new Node (data);
+    }
+    else 
+    {
+    RightNode.insert(data);
+    }
+        
     }
     
     
     }
+    
+    public boolean isContaine(int val){
+    if (val == data)
+        return true; 
+    else 
+    {
+    if (val< data){
+    if (LeftNode == null) 
+        return false ;
+    else 
+        LeftNode.isContaine(val);
+    
+    }
+    
+    else 
+    {
+    if (RightNode ==null){
+    
+    return false ;
+    }
+    return RightNode.isContaine(val);
+    
+    }
+    
+    }
+            
+            return false ;
+            
+    
+    }
+    
+
+    
+    
+    public void preOrder(){
+        System.out.println(data);
+        if (LeftNode!=null)
+             LeftNode.inOrder();
+  
+  
+     if (RightNode!=null)
+         RightNode.inOrder(); 
+            
+    }
+    public void inOrder(){
+    //// left root right
+    
+    if (LeftNode!=null)
+        LeftNode.inOrder();
+    
+    
+        System.out.println(data);
+        
+        
+     if (RightNode!=null)
+         RightNode.inOrder(); 
+    }
+    public void postOrder(){
+      if (LeftNode!=null)
+        LeftNode.inOrder();
+    
+             
+     if (RightNode!=null)
+         RightNode.inOrder(); 
+     
+      
+    System.out.println(data);
+    }
+    
+    
+    
     }
 
     
